bbced3a6cca7240890827110f25c10444c9829e8
Luis Martín <luismarmai@gmail.com>
2025-05-23 14:07:43 +0200

    Error fixing

--- src/components/ConfigurationModal.tsx
+++ src/components/ConfigurationModal.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import * as React from 'react';
+const { useState, useEffect } = React;
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogClose, DialogDescription } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
